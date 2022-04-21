011d0884780e2de1e702d90bdf927eafff84962b
Arlonas <98306152+Arlonas@users.noreply.github.com>
2022-04-21 14:36:14 +0700

    feat: added createproduct and get product by id

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,5 +1,46 @@
+const { Product } = require("../lib/sequelize");
+
 const productControllers = {
+  createProduct: async (req, res) => {
+    try {
+        const { product_name, price, description, image_url, stock } = req.body
 
-}
+        const newProduct = await Product.create({
+            image_url,
+            stock,
+            product_name,
+            price,
+            description
+        })
 
-module.exports = productControllers
+        return res.status(201).json({
+            message: "Product created",
+            result: newProduct
+        })
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        message: "Server error",
+      });
+    }
+  },
+  getProductById: async (req,res) => {
+      try {
+          const { id } = req.params
+
+          const findProductById = await Product.findByPk(id)
+
+          return res.status(200).json({
+              message: "Fetch product by id",
+              result: findProductById
+          })
+      } catch (err) {
+          console.log(err)
+          return res.status(500).json({
+              message: "Failed to fetch product by id"
+          })
+      }
+  }
+};
+
+module.exports = productControllers;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,3 +1,9 @@
+const productControllers = require("../controllers/products")
+
 const router = require("express").Router()
+
+router.get("/:id", productControllers.getProductById)
+
+router.post("/", productControllers.createProduct)
 
 module.exports = router
