73eff645b4042e6c6f0df86607fbd7226e7b3c7e
jrmygp <fat.snorlax17@gmail.com>
2022-04-21 16:18:01 +0700

    fixed merge conflict
    merge conflict fixed

--- src/routers/users.js
+++
@@ -1,9 +0,0 @@
-const { userController } = require("../controllers")
-
-const router = require("express").Router()
-
-router.post("/register", userController.registerUser)
-router.post("/login", userController.loginUser)
-
-
-module.exports = router
