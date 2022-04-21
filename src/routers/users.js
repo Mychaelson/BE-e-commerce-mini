1e9441fa3f60d8d68669d7e93eeb1b3de4c4d44d
jrmygp <fat.snorlax17@gmail.com>
2022-04-21 16:03:53 +0700

    feat: created login user end point
    user can now login with their existing account.

--- src/routers/users.js
+++ src/routers/users.js
@@ -3,6 +3,7 @@ const { userController } = require("../controllers")
 const router = require("express").Router()
 
 router.post("/register", userController.registerUser)
+router.post("/login", userController.loginUser)
 
 
 module.exports = router
