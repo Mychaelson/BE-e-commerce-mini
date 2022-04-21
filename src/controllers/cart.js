6a8b7e799f816f2428cf7082831acb41f00dd3fc
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-04-21 15:16:47 +0700

    Feat: added endpoint to add cart and get the cart item

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,3 +1,68 @@
-const cartController = {};
+const { Cart } = require("../lib/sequelize");
+
+const cartController = {
+  getCartItems: async (req, res) => {
+    try {
+      const cartItems = await Cart.findAll();
+
+      if (!cartItems.length) {
+        return res.status(400).json({
+          message: "No Items Found",
+        });
+      }
+
+      return res.status(200).json({
+        message: "Items in cart",
+        result: cartItems,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Server Error",
+      });
+    }
+  },
+  addCart: async (req, res) => {
+    try {
+      const { product_id, user_id, quantity = 1 } = req.body;
+
+      const findCart = await Cart.findOne({
+        where: {
+          product_id,
+          user_id,
+        },
+      });
+
+      if (findCart) {
+        await Cart.update(
+          {
+            quantity: quantity + findCart.dataValues.quantity,
+          },
+          {
+            where: {
+              product_id,
+              user_id,
+            },
+          }
+        );
+      } else {
+        await Cart.create({
+          product_id,
+          user_id,
+          quantity,
+        });
+      }
+
+      return res.status(200).json({
+        message: "Added Product to cart",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Server Error",
+      });
+    }
+  },
+};
 
 module.exports = cartController;
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,3 +1,9 @@
+const { cartController } = require("../controllers");
+
 const router = require("express").Router();
+
+router.get("/", cartController.getCartItems);
+
+router.post("/", cartController.addCart);
 
 module.exports = router;
