cbab9b6cfeaef9f83b36c27dc8eceaf6f7743eb2
jrmygp <fat.snorlax17@gmail.com>
2022-04-22 15:58:41 +0700

    FIX : Installed the middleware to user route (#12)
    * FEAT : Created authMiddleware
    
    * FIX : installed the middleware to user route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 const { userController } = require("../controllers")
+const authorizedLoggenInUser = require("../middlewares/authMiddleware")
 
 const router = require("express").Router()
 
 router.post("/register", userController.registerUser)
 router.post("/login", userController.loginUser)
-router.get("/refresh-token", userController.keepLogin)
+router.get("/refresh-token", authorizedLoggenInUser,userController.keepLogin)
 
 
 
